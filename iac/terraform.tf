e12c8fbd018fca133f8043ff6be71359d34956a0
Sake Pon <zzymukerzz@gmail.com>
2023-07-30 17:34:51 +0900

    terraform不要コード

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -3,10 +3,6 @@ data "aws_caller_identity" "current" {}
 locals {
   name = "app-runner-study"
 }
-data "aws_db_instance" "database" {
-  db_instance_identifier = "database-1"
-}
-
 resource "aws_ecr_repository" "ecr" {
   name                 = local.name
   image_tag_mutability = "MUTABLE"
